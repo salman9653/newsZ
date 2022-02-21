b6dd7a2f5e8869d68dab1f89ff38d3aed7b0a3e0
Salman <salman9915189734@gmail.com>
2022-02-22 00:09:08 +0530

    added sample news

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -4,8 +4,34 @@ import NewsItem from './NewsItem'
 export class News extends Component {
   render() {
     return (
-      <div>
-        <NewsItem />
+      <div className='container my-3'>
+        <h2>NewsZ - Top Headlines</h2>
+        <div className="row">
+          <div className="col-md-4">
+            <NewsItem 
+              title="News Title"
+              description="News Description goes Here"
+              imgUrl="https://imgix.bustle.com/uploads/image/2022/2/18/94601218-9f27-4d3b-8c5f-7128e8f095f1-destiny-2_-the-witch-queen-launch-trailer-0-21-screenshot.png?w=1200&h=630&fit=crop&crop=faces&fm=jpg"
+              url="https://www.inverse.com/gaming/destiny-2-witch-queen-launch-time-maintenance-update-size-twab"
+              />
+          </div>
+          <div className="col-md-4">
+            <NewsItem 
+              title="News Title"
+              description="News Description goes Here"
+              imgUrl="https://imgix.bustle.com/uploads/image/2022/2/18/94601218-9f27-4d3b-8c5f-7128e8f095f1-destiny-2_-the-witch-queen-launch-trailer-0-21-screenshot.png?w=1200&h=630&fit=crop&crop=faces&fm=jpg"
+              url="https://www.inverse.com/gaming/destiny-2-witch-queen-launch-time-maintenance-update-size-twab"
+              />
+          </div>
+          <div className="col-md-4">
+            <NewsItem 
+              title="News Title"
+              description="News Description goes Here"
+              imgUrl="https://imgix.bustle.com/uploads/image/2022/2/18/94601218-9f27-4d3b-8c5f-7128e8f095f1-destiny-2_-the-witch-queen-launch-trailer-0-21-screenshot.png?w=1200&h=630&fit=crop&crop=faces&fm=jpg"
+              url="https://www.inverse.com/gaming/destiny-2-witch-queen-launch-time-maintenance-update-size-twab"
+              />
+          </div>
+        </div>
       </div>
     )
   }
--- src/components/NewsItem.jsx
+++ src/components/NewsItem.jsx
@@ -2,9 +2,17 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
   render() {
+    let {imgUrl,title,description,url} = this.props;
     return (
-      <div>
-        News Item
+      <div className='my-3'>
+        <div className="card" style={{width: "20rem"}}>
+          <img src={imgUrl} className="card-img-top" alt="" />
+          <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{description}</p>
+            <a href={url} className="btn btn-sm btn-primary">Read More</a>
+          </div>
+        </div>
       </div>
     )
   }
