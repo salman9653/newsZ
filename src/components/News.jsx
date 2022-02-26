10c9c7bec162a8a22e9e5e1d0de6b7f95951bc8e
Salman <salman9915189734@gmail.com>
2022-02-26 20:30:47 +0530

    added Pre and Next button

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,27 +1,56 @@
 import React, { Component } from 'react';
 import NewsItem from './NewsItem';
+import MoreItem from './MoreItem';
 
 export class News extends Component {
   constructor(){
     super();
     this.state = {
       articles : [],
-      loading : false
+      loading : false,
+      page : 1
     }
   }
   async componentDidMount(){
-    let url= "https://newsapi.org/v2/top-headlines?country=in&apiKey=fd97076ba1a74c4288010115e4f2fbb4";
+    let url= "https://newsapi.org/v2/top-headlines?country=in&apiKey=fd97076ba1a74c4288010115e4f2fbb4&page=1&pageSize=18";
     let data= await fetch(url);
     let pasredData = await data.json()
-    this.setState({articles : pasredData.articles})
+    this.setState({articles : pasredData.articles , totalResults : pasredData.totalResults})
+  }
+  handelPre = async ()=>{
+    let url= `https://newsapi.org/v2/top-headlines?country=in&apiKey=fd97076ba1a74c4288010115e4f2fbb4&page=${this.state.page - 1}&pageSize=18`;
+    let data= await fetch(url);
+    let pasredData = await data.json()
+    this.setState({
+      page : this.state.page - 1,
+      articles : pasredData.articles
+  })
+  }
+  handelNext = async ()=>{
+    if(this.state.page+1 <= Math.ceil(this.state.totalResults/18)){
+      let url= `https://newsapi.org/v2/top-headlines?country=in&apiKey=fd97076ba1a74c4288010115e4f2fbb4&page=${this.state.page + 1}&pageSize=18`;
+      let data= await fetch(url);
+      let pasredData = await data.json()
+      this.setState({
+        page : this.state.page + 1,
+        articles : pasredData.articles
+    })    
+    }
+    else {}
   }
 
   render() {
     return (
       <div className='container my-3'>
-        <h1 className='p-2'>Top Headlines</h1>
-        <div className="row">
 
+        <h1 className='p-2'>Top Headlines</h1>
+
+        <div className="container d-flex justify-content-between">
+          <button disabled={this.state.page<=1} className='btn btn-primary' onClick={this.handelPre}>&#10094; Privious</button>
+          <button className='btn btn-success' onClick={this.handelNext}>Next &#10095;</button>
+        </div>
+
+        <div className="row">
           {this.state.articles.map( (element) => {
             return(
               <div className="col-lg-4 col-md-6 col-sm-12" key={element.url}>
@@ -34,7 +63,11 @@ export class News extends Component {
               </div>
             )
           })}
+        </div>
 
+        <div className="container d-flex justify-content-between">
+          <button disabled={this.state.page<=1} className='btn btn-primary' onClick={this.handelPre}>&#10094; Privious</button>
+          <button className='btn btn-success' onClick={this.handelNext}>Next &#10095;</button>
         </div>
       </div>
     )
--- src/components/NewsItem.jsx
+++ src/components/NewsItem.jsx
@@ -8,8 +8,8 @@ export class NewsItem extends Component {
         <div className="card" style={{width: "20rem"}}>
           <img src={imgUrl ? imgUrl : "https://cdn4.vectorstock.com/i/1000x1000/12/78/live-news-logo-flat-style-vector-20901278.jpg"} className="card-img-top" alt="" />
           <div className="card-body">
-            <h5 className="card-title">{title}...</h5>
-            <p className="card-text">{description}...</p>
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{description}</p>
             <a href={url} className="btn btn-sm btn-primary">Read More</a>
           </div>
         </div>
