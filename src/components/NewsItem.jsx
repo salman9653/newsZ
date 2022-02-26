186a58e95f6e7ded75552f1769164a0d42eea4aa
Salman <salman9915189734@gmail.com>
2022-02-26 15:21:55 +0530

    added newsItems from sample news

--- src/components/NewsItem.jsx
+++ src/components/NewsItem.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 export class NewsItem extends Component {
   render() {
@@ -8,8 +8,8 @@ export class NewsItem extends Component {
         <div className="card" style={{width: "20rem"}}>
           <img src={imgUrl} className="card-img-top" alt="" />
           <div className="card-body">
-            <h5 className="card-title">{title}</h5>
-            <p className="card-text">{description}</p>
+            <h5 className="card-title">{title}...</h5>
+            <p className="card-text">{description}...</p>
             <a href={url} className="btn btn-sm btn-primary">Read More</a>
           </div>
         </div>
