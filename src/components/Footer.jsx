2f64c41cf22dbf0bd7ec4984f253c74d78928632
Salman <salman9915189734@gmail.com>
2022-02-27 23:15:46 +0530

    added routers

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export class Footer extends Component {
+  render() {
+    let {imgUrl,title,description,url} = this.props;
+    return (
+        <div class="footer text-center" style={{backgroundColor:"black" , color : "white" ,padding:"10px    "}}>
+            <p> Copyright ©2022 | All rights reserved  </p>
+            <p> NewZ by Salman</p>
+        </div>
+    )
+  }
+}
+
+export default Footer;
--- src/components/NewsItem.jsx
+++ src/components/NewsItem.jsx
@@ -5,10 +5,10 @@ export class NewsItem extends Component {
     let {imgUrl,title,description,url} = this.props;
     return (
       <div className='my-3'>
-        <div className="card">
+        <div className="card" data-bs-toggle="tooltip" data-bs-placement="top" title={title}>
           <img src={imgUrl ? imgUrl : "https://cdn4.vectorstock.com/i/1000x1000/12/78/live-news-logo-flat-style-vector-20901278.jpg"} className="card-img-top" alt="" />
           <div className="card-body">
-            <h5 className="card-title">{title}</h5>
+            <h5 className="card-title">{title.slice(0,30)}...</h5>
             <p className="card-text">{description}</p>
             <a href={url} className="btn btn-sm btn-primary">Read More</a>
           </div>
